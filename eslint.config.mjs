f083164b1c630b89ed1c1b09e7e05369c6762260
Felipe Bravo <bravoback@outlook.com>
2026-05-12 12:36:19 -0300

    chore: setup shadcn ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,21 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-  {
-    ignores: [
-      'src/generated/prisma/**',
-      'pgdata/**',
-      '.next/**',
-      'node_modules/**',
-    ],
-  },
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'src/generated/prisma/**',
+    'pgdata/**',
+    'node_modules/**',
+  ]),
+
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     rules: {
@@ -22,6 +23,6 @@ const eslintConfig = [
       'prefer-arrow-callback': 'off',
     },
   },
-];
+]);
 
 export default eslintConfig;
